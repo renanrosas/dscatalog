950baa3826f61bbd56f804b19c4197b02e50db58
Renan Rosas <renan-rosas@hotmail.com>
2023-01-25 14:52:10 -0300

    POST new category

--- backend/src/main/java/com/renanrosas/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/renanrosas/dscatalog/controllers/CategoryController.java
@@ -2,14 +2,12 @@ package com.renanrosas.dscatalog.controllers;
 
 import com.renanrosas.dscatalog.dto.CategoryDTO;
 import com.renanrosas.dscatalog.services.CategoryService;
-import com.renanrosas.dscatalog.services.exceptions.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,5 +27,13 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
