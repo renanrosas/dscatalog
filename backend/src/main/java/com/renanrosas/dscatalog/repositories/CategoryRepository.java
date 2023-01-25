dd44201e1afc9f2eb115e79dbb03f2e70e0e74c0
Renan Rosas <renan-rosas@hotmail.com>
2023-01-25 15:40:02 -0300

    Update category

--- backend/src/main/java/com/renanrosas/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/renanrosas/dscatalog/repositories/CategoryRepository.java
@@ -2,8 +2,6 @@ package com.renanrosas.dscatalog.repositories;
 
 import com.renanrosas.dscatalog.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
