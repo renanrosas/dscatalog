afe9c1d8277e65ab90d37dc8e47f379e21131d4a
renanrosas <renan-rosas@hotmail.com>
2025-08-05 11:35:33 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/renanrosas/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/renanrosas/dscatalog/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +20,17 @@ public class CategoryController {
     this.categoryService = categoryService;
   }
 
-
   @GetMapping
   public ResponseEntity<List<CategoryDTO>> findAll() {
     // This method will return all categories;
     List<CategoryDTO> list = categoryService.findAll();
     return ResponseEntity.ok().body(list);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+    // This method will return a category by its ID;
+    CategoryDTO dto = categoryService.findById(id);
+    return ResponseEntity.ok().body(dto);
   }
 }
--- backend/src/main/java/com/renanrosas/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/renanrosas/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.renanrosas.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.renanrosas.dscatalog.dto.CategoryDTO;
 import com.renanrosas.dscatalog.entities.Category;
 import com.renanrosas.dscatalog.repositories.CategoryRepository;
+import com.renanrosas.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,5 +24,13 @@ public class CategoryService {
     // This method will return all categories;
     List<Category> list = categoryRepository.findAll();
     return list.stream().map(CategoryDTO::new).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public CategoryDTO findById(Long id) {
+    // This method will return a category by its ID;
+    Optional<Category> obj = categoryRepository.findById(id);
+    Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+    return new CategoryDTO(entity);
   }
 }
